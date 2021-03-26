c5e1d528c5abc3fa433d80688cf46ab70be0a88b
Taehong Kim <lkoiescg2031@naver.com>
2021-03-27 00:22:55 +0900

    [FIX] Post에서 Media 이미지가 나오지 않는 오류 수정
    [Modify] Post의 미리보기 Content UI 변경
    
    [Doc] Post source code 언어 설정 변경
    
      - =c++ => cpp
      - =javascript => javascript
    
    [FIX] 사용하지 않는 utils 모듈 제거

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import clsx from 'clsx';
+
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -14,13 +16,25 @@ import Button from '@material-ui/core/Button';
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 
 import Tags from './Tags';
+import { getRandomInt } from '../utils/math';
 
 const usePostStyle = makeStyles(theme => ({
+  cardWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+  },
   cardRoot: {
     width: '300px',
     padding: theme.spacing(1),
     margin: theme.spacing(1),
     marginBottom: theme.spacing(3),
+  },
+  cardContent: {
+    maxHeight: '260px',
+    overflow: 'hidden',
+  },
+  cardContentWithMedia: {
+    maxHeight: '100px',
   },
   media: {
     height: 0,
@@ -31,48 +45,77 @@ const usePostStyle = makeStyles(theme => ({
   },
 }));
 
+const domParser = new DOMParser();
+
 function Post({ post }) {
   const classes = usePostStyle();
-  const descLen = 150 + (post.frontmatter.featuredImage ? 0 : 350);
+
+  const imgTagRegExp = /<img[^>]*>?/gm;
+  const desc = post.html.replace(imgTagRegExp, '');
+
+  const imgHtmlStrings = post.html.match(imgTagRegExp) || new Array(0);
+  const descImgs = imgHtmlStrings.map(imgStr => {
+    const $img = domParser
+      .parseFromString(imgStr, 'application/xml')
+      .getElementsByTagName('img')[0];
+    return $img.attributes['src'].value;
+  });
+
+  // 대표 이미지가 설정된 경우 사용
+  let mediaImage = post.frontmatter.featuredImage;
+  if (mediaImage !== null) {
+    mediaImage = mediaImage.childImageSharp.fluid.originalImg;
+  }
+  // 본문안에 이미지가 있는 경우 사용 랜덤 값 사용
+  else if (descImgs.length > 0) {
+    mediaImage = descImgs[getRandomInt(0, descImgs.length)];
+  }
+
   return (
-    <Card elevation={3} classes={{ root: classes.cardRoot }}>
-      <CardHeader
-        title={
-          <>
-            <Typography align="right" variant="caption">
-              <CalendarTodayIcon fontSize="inherit" />
-              {new Date(post.frontmatter.date).toLocaleDateString()}
-            </Typography>
-            <Typography variant="h6">{post.frontmatter.title}</Typography>
-            <Tags outlined tags={post.frontmatter.tags} />
-          </>
-        }
-      />
-      {post.frontmatter.featuredImage && (
-        <CardMedia
-          className={classes.media}
-          image={`./${post.frontmatter.featuredImage.childImageSharp.fluid.originalImg}`}
+    <div className={classes.cardWrapper}>
+      <Card elevation={3} classes={{ root: classes.cardRoot }}>
+        <CardHeader
           title={
-            post.frontmatter.featuredImage.childImageSharp.fluid.originalImg
+            <>
+              <Typography align="right" variant="caption">
+                <CalendarTodayIcon fontSize="inherit" />
+                {new Date(post.frontmatter.date).toLocaleDateString()}
+              </Typography>
+              <Typography variant="h6">{post.frontmatter.title}</Typography>
+              <Tags outlined tags={post.frontmatter.tags} />
+            </>
           }
         />
-      )}
-      <CardContent>
-        <Typography variant="body2">
-          {post.html.replace(/<[^>]*>?/gm, '').substring(0, descLen)}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button
-          variant="contained"
-          color="secondary"
-          href={post.fields.slug}
-          classes={{ root: classes.showButtonRoot }}
-        >
-          SHOW
-        </Button>
-      </CardActions>
-    </Card>
+        {mediaImage && (
+          <CardMedia
+            className={classes.media}
+            image={mediaImage}
+            title={mediaImage}
+          />
+        )}
+        <CardContent>
+          <Typography
+            variant="body2"
+            classes={{
+              root: clsx(classes.cardContent, {
+                [classes.cardContentWithMedia]: mediaImage !== null,
+              }),
+            }}
+            dangerouslySetInnerHTML={{ __html: desc }}
+          />
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="contained"
+            color="secondary"
+            href={post.fields.slug}
+            classes={{ root: classes.showButtonRoot }}
+          >
+            SHOW
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
 
--- src/posts/Language/Javascript/typehinting.md
+++ src/posts/Language/Javascript/typehinting.md
@@ -11,7 +11,7 @@ log: [
 Javascript`에서는 변수의 타입을 선언하지 않아 타입과 관련된 에러가 많이 발생할 수 있다. 이를 방지하고자 타입 힌트를 사용하는 데 사용방법은 다음과 같다.
 
 ---
-````=javascript
+```javascript
 /**
 * @return {undefined}
 * @param {{foo: string, bar: number}} obj
@@ -21,13 +21,13 @@ function func(obj) {
     var bar = obj.bar;
     //obj. 여기까지만 타이핑하면 프로퍼티 foo, bar가 존재함을 알 수 있다!
 }
-````
+```
 ---
 
 또한 제네릭 타입을 이용하여 타입 힌트를 줄 수도 있다.
 
 ---
-```=javascript
+```javascript
 /**
  * @template T
  */
